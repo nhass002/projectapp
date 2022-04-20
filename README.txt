b6eaf91750ba39885acbce297120d69123f91ffc
nhass002 <92883243+nhass002@users.noreply.github.com>
2022-04-20 15:33:08 +0100

    fixing commit

--- README.txt
+++ README.txt
@@ -1,6 +1,9 @@
 opencv needs to be pip installed
 pyqt5 needs to be installed
+numpy needs to be installed
+
+download the weights file and keep it with main.py https://drive.google.com/file/d/1YqQx4CGi1ijba7KBwqz0Efnb2wePNOEi/view?usp=sharing
 
 1. Run on code
-2. Hold up an apple to the camera or just use recognition on your face
+2. Hold up objects to the camera or just use recognition on your face/body
 3. Click X on the window to end the program
--- main.py
+++ main.py
@@ -11,7 +11,30 @@ from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt,QObject, QThread
 import sys
 import numpy as np
 
-#https://www.imagetracking.org.uk/2020/12/displaying-opencv-images-in-pyqt/
+classesFile = 'coco.names'
+classNames = []
+
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+#print(classNames)
+#print(len(classNames))
+#print
+"""
+modelConfiguration = 'yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+widthHeight = 320
+
+confThreshold = 0.5
+nms = 0.3
+"""
+
+
+#https://www.imagetracking.org.uk/2020/12/displaying-opencv-images-in-pyqt/ THIS IS ONLY FOR DISPLAYING IMAGES IN GUI
 class Thread(QThread):
     change_pixmap_signal = pyqtSignal(np.ndarray) #uses numpy to turn the matrix into an array
     """
@@ -19,83 +42,71 @@ class Thread(QThread):
         self._running = True
 
     def terminate(self):
-        self._running = False
+        self._running = False 
     """
-
+#test
     def run(self):
         # capture from web cam
         capture = cv2.VideoCapture(0)
+        self.modelConfiguration = 'yolov3.cfg'
+        self.modelWeights = 'yolov3.weights'
+
+        self.net = cv2.dnn.readNetFromDarknet(self.modelConfiguration, self.modelWeights)
+        self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+        self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+        self.widthHeight = 480
+
+        self.confThreshold = 0.5
+        self.nms = 0.3
+
         while True: #while self._running
-            self.fruits_cascade = cv.cv2.CascadeClassifier('updated_haar_images/fruitcascade.xml')
-            self.apples_cascade = cv.cv2.CascadeClassifier('updated_haar_images/applecascade.xml')
-            #self.bananas_cascade = cv.cv2.CascadeClassifier('updated_haar_images/bananacascade.xml')
-            # -- Detect faces
-            self.face_cascade = cv2.CascadeClassifier('venv\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
 
             check, cv_img = capture.read() #update frames
-            cv_img = cv.flip(cv_img, 1)
-            if check:
+            #cv_img = cv.flip(cv_img, 1)
+            #if check:
+            self.blob = cv2.dnn.blobFromImage(cv_img, 1 / 255.0, (self.widthHeight, self.widthHeight), [0, 0, 0], 1, crop=False)
+            self.net.setInput(self.blob)
 
-                gray = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+            self.layerNames = self.net.getLayerNames()
 
-                fruitcascades = self.fruits_cascade.detectMultiScale(gray, 1.01, 7)
-                applecascades = self.apples_cascade.detectMultiScale(gray, 1.01, 7)
-                # bananacascades = bananas_cascade.detectMultiScale(gray, 1.01, 7)
-                #face cascade below
-                faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
-                #detecting code below
-                """
-                for (x, y, w, h) in fruitcascades:
-                    cv_img = cv.cv2.rectangle(cv_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    cv.cv2.putText(cv_img, 'fruits', ((x + w) - 10, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30),
-                                   2)
-                for (x, y, w, h) in applecascades:
-                    cv_img = cv.cv2.rectangle(cv_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    cv.cv2.putText(cv_img, 'apple', ((x + w) - 30, (y + h) - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
-                                   (30, 255, 30), 2)
-                # for (x, y, w, h) in bananacascades:
-                # cv_img = cv.cv2.rectangle(cv_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                # cv.cv2.putText(cv_img, 'banana', (x, (y + h) - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30), 2)
-                """
+            self.outputNames = [self.layerNames[i - 1] for i in self.net.getUnconnectedOutLayers()]
+            self.outputs = self.net.forward(self.outputNames)
 
-                #Draw rectangle around the faces
-                for (x, y, w, h) in faces:
-                    cv_img = cv2.rectangle(cv_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    cv.cv2.putText(cv_img, 'face', (x, (y + h) - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30), 2)
+            self.findObjects(self.outputs, cv_img)
+            self.change_pixmap_signal.emit(cv_img)
 
-                self.change_pixmap_signal.emit(cv_img)
-                #self.detect(cv_img) #this breaks the camera when running
-    """
-    def detect(self,frame):
-        dscascades = self.three_ds_cascade.detectMultiScale(frame, 1.01,
-                                                       7)  # holds the classifier multiscale which does the detecting
-        # and returns the boundaries for the rectangle
-        # fruitcascades = fruits_cascade.detectMultiScale(frame, 1.01, 7)
-        fruitcascades = self.fruits_cascade.detectMultiScale(frame, 1.01, 7)
-        applecascades = self.apples_cascade.detectMultiScale(frame, 1.01, 7)
-        #bananacascades = bananas_cascade.detectMultiScale(frame, 1.01, 7)
+            #cv2.imshow('image', cv_img)
+            #cv2.waitKey(1)
 
-        for (x, y, w, h) in fruitcascades:
-            frame = cv.cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv.cv2.putText(frame, 'fruits', ((x + w) - 10, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30), 2)
-        for (x, y, w, h) in applecascades:
-            frame = cv.cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv.cv2.putText(frame, 'apple', ((x + w) - 30, (y + h) - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30), 2)
-        #for (x, y, w, h) in bananacascades:
-            #frame = cv.cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            #cv.cv2.putText(frame, 'banana', (x, (y + h) - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30), 2)
+    def findObjects(self,outputs, img):
+        height, width, center = img.shape
+        bound = []
+        classIDs = []
+        confidence = []
 
-        # -- Detect faces
-        face_cascade = cv2.CascadeClassifier('venv\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # Detect faces
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-        # Draw rectangle around the faces
-        for (x, y, w, h) in faces:
-            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv.cv2.putText(frame, 'face', (x, (y + h) - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30), 2)
-    """
+        for output in outputs:
+            for detection in output:
+                scores = detection[5:]  # first 5 are removed
+                classID = np.argmax(scores)  # max value
+                conf = scores[classID]
+                if conf > self.confThreshold:
+                    w, h = int(detection[2] * width), int(detection[3] * height)
+                    x, y = int((detection[0] * width) - w / 2), int((detection[1] * height) - h / 2)
+                    bound.append([x, y, w, h])
+                    classIDs.append(classID)
+                    confidence.append(float(conf))
 
+        # print(len(bound))
+        indices = cv2.dnn.NMSBoxes(bound, confidence, self.confThreshold, self.nms)
+        # print(indices)
+        for i in indices:
+            # i = i[0]
+            box = bound[i]
+            x, y, w, h = box[0], box[1], box[2], box[3]
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.putText(img, f'{classNames[classIDs[i]].upper()} {int(confidence[i] * 100)}%',
+                        (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
 
 #source of code that is adapted from https://www.imagetracking.org.uk/2020/12/displaying-opencv-images-in-pyqt/
 #source 2 https://github.com/docPhil99/opencvQtdemo/blob/master/staticLabel2.py
@@ -113,19 +124,17 @@ class mainWindow(QWidget):
         # create a text label
         self.textLabel = QLabel('Menu')
 
-        #Below here are the cascades
-        #self.three_ds_cascade = cv.cv2.CascadeClassifier(
-            #'updated_haar_images/classifier/cascade.xml')  # finds the classifier in the path
+        self.modelConfiguration = 'yolov3.cfg'
+        self.modelWeights = 'yolov3.weights'
 
-        #self.fruits_cascade = cv.cv2.CascadeClassifier('updated_haar_images/fruitcascade.xml')
-        #self.apples_cascade = cv.cv2.CascadeClassifier('updated_haar_images/applecascade.xml')
-        #self.bananas_cascade = cv.cv2.CascadeClassifier('updated_haar_images/bananacascade.xml')
+        self.net = cv2.dnn.readNetFromDarknet(self.modelConfiguration, self.modelWeights)
+        self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+        self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
-        self.fruits_cascade = cv.cv2.CascadeClassifier('updated_haar_images/fruitcascade.xml')
-        self.apples_cascade = cv.cv2.CascadeClassifier('updated_haar_images/applecascade.xml')
-        # self.bananas_cascade = cv.cv2.CascadeClassifier('updated_haar_images/bananacascade.xml')
-        # -- Detect faces
-        self.face_cascade = cv2.CascadeClassifier('venv\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
+        self.widthHeight = 320
+
+        self.confThreshold = 0.5
+        self.nms = 0.3
 
         widget = QWidget()
         #add 2 frames here
@@ -141,7 +150,11 @@ class mainWindow(QWidget):
         #self.pushButton.setGeometry(QtCore.QRect(10, 10, 75, 23)) #uncomment this later
         self.pushButton.setObjectName("pushButton")
         self.pushButton.setText("Switch Mode")
-        self.mode = 0
+
+        #-----------------
+        self.mode = 0 # change mode manually at start here
+        #---------------------
+
 
         #screenshot
         self.pushButton_2 = QtWidgets.QPushButton(widget)
@@ -190,8 +203,21 @@ class mainWindow(QWidget):
             # set the image image to the grey pixmap
             self.image_label.setPixmap(grey)
             self.img = cv.imread("updated_haar_images/test_files_grayscale/apple_80.jpg")
+
+            #
+            self.blob = cv2.dnn.blobFromImage(self.img, 1 / 255.0, (self.widthHeight, self.widthHeight), [0, 0, 0], 1,
+                                              crop=False)
+            self.net.setInput(self.blob)
+
+            self.layerNames = self.net.getLayerNames()
+
+            self.outputNames = [self.layerNames[i - 1] for i in self.net.getUnconnectedOutLayers()]
+            self.outputs = self.net.forward(self.outputNames)
+
+
             # perform detection on the image
-            self.detect(self.img)
+            #self.detect(self.img)
+            self.findObjects(self.outputs, self.img)
             # convert the image to Qt format
             qt_img = self.convert_cv_qt(self.img)
             # display it
@@ -222,8 +248,20 @@ class mainWindow(QWidget):
             # set the image image to the grey pixmap
             self.image_label.setPixmap(grey)
             self.img = cv.imread("updated_haar_images/test_files_grayscale/apple_80.jpg")
+            #
+            self.blob = cv2.dnn.blobFromImage(self.img, 1 / 255.0, (self.widthHeight, self.widthHeight), [0, 0, 0], 1,
+                                              crop=False)
+            self.net.setInput(self.blob)
+
+            self.layerNames = self.net.getLayerNames()
+
+            self.outputNames = [self.layerNames[i - 1] for i in self.net.getUnconnectedOutLayers()]
+            self.outputs = self.net.forward(self.outputNames)
+
+
             # perform detection on the image
-            self.detect(self.img)
+            #self.detect(self.img)
+            self.findObjects(self.outputs, self.img)
             # convert the image to Qt format
             qt_img = self.convert_cv_qt(self.img)
             # display it
@@ -260,14 +298,37 @@ class mainWindow(QWidget):
     #function for uploading image button
     @pyqtSlot()
     def upload_image(self):
-        #self.filename = QtGui.QFileDialog.getOpenFileName(self,'Open File', '.')
-        #self.filename = QtGui.QFileDialog.getExistingDirectory(None, 'Select a folder:', 'C:\\', QtGui.QFileDialog.ShowDirsOnly)
-        self.filename = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\', "Image files (*.jpg *.gif)")
-        self.img = cv.imread(self.filename) #self.file_image
-        self.update_image(self.file_image)
-        #read image file here
-        #call update_image and pass the image
-        print("e")
+        #print("e")
+        self.filename = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\')
+        self.path = self.filename[0]
+
+        #print(self.filename)
+        #print(self.path)
+
+        grey = QPixmap(self.disply_width, self.display_height)
+        grey.fill(QColor('darkGray'))
+        # set the image to the grey pixmap
+        self.image_label.setPixmap(grey)
+        # read image file here
+        self.img = cv.imread(self.path) #self.file_image
+
+        #
+        self.blob = cv2.dnn.blobFromImage(self.img, 1 / 255.0, (self.widthHeight, self.widthHeight), [0, 0, 0], 1,
+                                          crop=False)
+        self.net.setInput(self.blob)
+
+        self.layerNames = self.net.getLayerNames()
+
+        self.outputNames = [self.layerNames[i - 1] for i in self.net.getUnconnectedOutLayers()]
+        self.outputs = self.net.forward(self.outputNames)
+
+        # perform detection on the image
+        # self.detect(self.img)
+        self.findObjects(self.outputs, self.img)
+        # convert the image to Qt format
+        qt_img = self.convert_cv_qt(self.img)
+        # display it
+        self.image_label.setPixmap(qt_img)
 
     #function for screenshotting images
 
@@ -280,6 +341,7 @@ class mainWindow(QWidget):
         elif self.mode == 1: #video
             self.video_label.setPixmap(qt_img)
         #detect(qt_img)
+        #self.findObjects(self.outputs, self.img)
 
     def convert_cv_qt(self, cv_img):
         #Convert from an opencv image to QPixmap
@@ -290,12 +352,35 @@ class mainWindow(QWidget):
         p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
 
-    #detection code used to be here
-    #self.fruits_cascade = cv.cv2.CascadeClassifier('updated_haar_images/fruitcascade.xml')
-    #self.apples_cascade = cv.cv2.CascadeClassifier('updated_haar_images/applecascade.xml')
-    # self.bananas_cascade = cv.cv2.CascadeClassifier('updated_haar_images/bananacascade.xml')
-    # -- Detect faces
-    #self.face_cascade = cv2.CascadeClassifier('venv\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
+    def findObjects(self,outputs, img):
+        height, width, center = img.shape
+        bound = []
+        classIDs = []
+        confidence = []
+
+        for output in outputs:
+            for detection in output:
+                scores = detection[5:]  # first 5 are removed
+                classID = np.argmax(scores)  # max value
+                conf = scores[classID]
+                if conf > self.confThreshold:
+                    w, h = int(detection[2] * width), int(detection[3] * height)
+                    x, y = int((detection[0] * width) - w / 2), int((detection[1] * height) - h / 2)
+                    bound.append([x, y, w, h])
+                    classIDs.append(classID)
+                    confidence.append(float(conf))
+
+        # print(len(bound))
+        indices = cv2.dnn.NMSBoxes(bound, confidence, self.confThreshold, self.nms)
+        # print(indices)
+        for i in indices:
+            # i = i[0]
+            box = bound[i]
+            x, y, w, h = box[0], box[1], box[2], box[3]
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            cv2.putText(img, f'{classNames[classIDs[i]].upper()} {int(confidence[i] * 100)}%',
+                        (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+    """
     def detect(self, frame):
         #dscascades = self.three_ds_cascade.detectMultiScale(frame, 1.01,
                                                             #7)  # holds the classifier multiscale which does the detecting
@@ -338,6 +423,7 @@ apples_cascade = cv.cv2.CascadeClassifier('updated_haar_images/applecascade.xml'
 bananas_cascade = cv.cv2.CascadeClassifier('updated_haar_images/bananacascade.xml')
 
 #mode = 1;
+    """
 
 
 if __name__ == "__main__":
@@ -376,7 +462,7 @@ while True:
             frame = cv.flip(frame, 1) #flips the camera so that it acts like a mirror
             #windowcapture = "Capture"
             detect(frame)
-    """
+    
 
     def detect(frame):
         dscascades = three_ds_cascade.detectMultiScale(frame, 1.01,
@@ -386,14 +472,14 @@ while True:
         fruitcascades = fruits_cascade.detectMultiScale(frame, 1.01, 7)
         applecascades = apples_cascade.detectMultiScale(frame, 1.01, 7)
         bananacascades = bananas_cascade.detectMultiScale(frame, 1.01, 7)
-        """
+        
         for (x, y, w, h) in dscascades:
             # gray = cv.cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
             frame = cv.cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
             # frame as video, x and y for the top left corner, x+w and y+h will get the bottom corner, colour blue and the line thickness
             cv.cv2.putText(frame, '3DS', (x, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30), 2)
             # takes in the frame image as parameter, labels 3ds above the rectangle, y-2 would let it sit on the rectangle, font, font scaling, font colour and font thickness
-        """
+        
         for (x, y, w, h) in fruitcascades:
             frame = cv.cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
             cv.cv2.putText(frame, 'fruits', ((x + w) - 10, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30), 2)
@@ -423,7 +509,7 @@ while True:
         for (x, y, w, h) in faces:
             frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
             cv.cv2.putText(frame, 'face', (x, (y + h) - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (30, 255, 30), 2)
-    """
+    
     if mode == 0:
         img = cv.imread("updated_haar_images/test_files_grayscale/apple_80.jpg") #apple_78
         #img = cv.imread("updated_haar_images/test/mixed_6.jpg")
@@ -439,7 +525,7 @@ while True:
         frame = cv.flip(frame, 1) #flips the camera so that it acts like a mirror
         windowcapture = "Capture"
         ui.detect(frame)
-    """
+    
 
     def greyscale(frame):
             #Camera but grayscaled, decided not to use this yet
@@ -475,6 +561,8 @@ sys.exit(mainWindow.exec_())
     #ui.setupUi(MainWindow)
     #MainWindow.show()
     #sys.exit(app.exec_())
+    
+"""
 
 
 
